8db1c3a35f682f558060d1cea6d40e07fa441b0e
黄勇强 <66911712+snakehyq@users.noreply.github.com>
2020-08-21 20:26:43 +0800

    Add files via upload

--- src/views/yunke/studio/task/items/Edit.vue
+++ src/views/yunke/studio/task/items/Edit.vue
@@ -3,8 +3,8 @@
     <el-dialog
     :title="title"
     :width="width"
-    top=""
-    close-on-click-modal
+    top="3vh"
+    :close-on-click-modal="false" 
     :close-on-press-escape="false"
     :visible.sync="isVisible"
   >
@@ -16,22 +16,24 @@
         <el-input v-model="tasks.introduction"  />
       </el-form-item>
        <el-form-item label="开始时间">
-        <el-col :span="15">
-          <el-date-picker
-            v-model="tasks.startTime"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="选择日期">
-          </el-date-picker>
-        </el-col>
+        <el-date-picker
+          v-model="tasks.startTime"
+          type="date"
+          value-format="yyyy-MM-dd"
+          placeholder="选择日期"
+          style="width:100%"
+          >
+        </el-date-picker>
       </el-form-item>
       <el-form-item label="结束时间">
-        <el-col :span="15">
+        <el-col>
           <el-date-picker
             v-model="tasks.endTime"
             type="date"
             value-format="yyyy-MM-dd"
-            placeholder="选择日期">
+            placeholder="选择日期"
+            style="width:100%"
+            >
           </el-date-picker>
         </el-col>
       </el-form-item>
@@ -83,19 +85,21 @@
       <el-form-item label="发票" prop="invoice">
         <!-- <el-button size="small" type="primary" @click="upload('发票')">点击上传</el-button> -->
         <el-upload
-          :before-upload="handleBeforeUpload"
-          :before-remove="handleBeforeRemove"
+          :before-upload="handleBeforeUploadInvoice"
+          :before-remove="addInvoiceBeforeRemove"
           :on-success="handleSuccessInvoice"
-          :file-list="fileList"
+          :file-list="invoiceFileList"
           :action="uploadUrl"
-          class="upload-demo"
+          :class="{hideupload:editrepairinvoicehideupload, picturecard:true}"
+          accept=".jpg,.jpeg,.png,.gif,.bmp"
           :headers="headers"
           multiple
-          :limit="3"
+          list-type="picture-card"
+          :limit="uploadPicLimit"
           drag
+          :on-preview="handlePreview"
         >
           <i  class="el-icon-upload" />
-          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
           <div slot="tip" style="display: block;" class="el-upload__tip">请勿上传违法文件，可同时上传3个附件，且文件不超过5M</div>
         </el-upload>
       </el-form-item>
@@ -103,28 +107,43 @@
        <!-- <el-input v-model="tasks.ce" /> -->
        <!-- <el-button size="small" type="primary" @click="upload('项目说明书')">点击上传</el-button> -->
          <el-upload
-          :before-upload="handleBeforeUpload"
+          :before-upload="handleBeforeUploadSpecif"
           :before-remove="handleBeforeRemove"
           :on-success="handleSuccessSpecification"
           :file-list="fileList"
           :action="uploadUrl"
-          class="upload-demo"
+          accept=".doc,.docx,.pdf,.ppt"
+          :class="{hideupload:editrepairinvoicehideupload, picturecard:true}"
           :headers="headers"
-          multiple
-          :limit="1"
+          :limit="uploadDocTarLimit"
           drag
         >
           <i  class="el-icon-upload" />
-          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-          <div slot="tip" style="display: block;" class="el-upload__tip">请勿上传违法文件，可同时上传3个附件，且文件不超过5M</div>
+          <div slot="tip" style="display: block;" class="el-upload__tip">请勿上传违法文件，可同时上传1个附件，且文件不超过5M</div>
         </el-upload>
-    </el-form-item> 
+       </el-form-item> 
        <el-form-item label="源文件" prop="url">
         <el-input v-model="tasks.url" />
+        <!-- <el-upload
+          :before-upload="handleBeforeUploadUrl"
+          :before-remove="handleBeforeRemove"
+          :on-success="handleSuccessUrl"
+          :file-list="fileList"
+          :action="uploadUrl"
+          accept=".doc,.pdf,.docx,.ppt,.rar,.zip,.arj,.gz,.tar,.tar.gz"
+          :class="{hideupload:editrepairinvoicehideupload, picturecard:true}"
+          :headers="headers"
+          :limit="uploadDocTarLimit"
+          list-type="picture-card"
+          drag
+        >
+          <i  class="el-icon-upload" />
+          <div slot="tip" style="display: block;" class="el-upload__tip">请勿上传违法文件，可同时上传1个附件，且文件不超过10M</div>
+        </el-upload> -->
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
-      <el-button type="warning" plain :loading="buttonLoading" @click="isVisible = false">
+      <el-button type="primary" plain :loading="buttonLoading" @click="isVisible = false">
         {{ $t('common.cancel') }}
       </el-button>
       <el-button type="primary" plain :loading="buttonLoading" @click.prevent="submitForm">
@@ -132,13 +151,17 @@
       </el-button>
     </div>
   </el-dialog>
+  <!-- 图片预览 -->
+  <el-dialog title="图片预览" :visible.sync="previewVisible" width="40%" @close="previewDialogClose">
+    <el-image :src="previewPath" alt class="previewImg" />
+    </el-dialog>
 </div>
 </template>
 <script>
 import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 import { qiNiuUrl } from '@/settings'
 import { getToken } from '@/utils/auth'
-import { validateFileExt } from '@/utils/my-validate'
+import {validateFileDocmentExt, validateFileExt, validatePicExt, validateFileTarExt } from '@/utils/my-validate'
 
 export default {
   name: 'ItemsEdit',
@@ -154,15 +177,27 @@ export default {
   },
   data() {
     return {
+      // 上传图片数量限制
+      uploadPicLimit: 3,
+      // 上传文档和压缩包数量限制
+      uploadDocTarLimit: 1,
+      // 超过图片数量限制时隐藏上传组件
+      editrepairinvoicehideupload: false,
       headers: {
         Authorization: `bearer ${getToken()}`
       },
       uploadUrl: qiNiuUrl,
       initFlag: false,
+      // 原始图片数据
       fileList: [],
       files: [],
+      // 发票上传的数据
+      invoiceFiles: [],
+      invoiceFileList: [],
+      addInvoiceListLength: 0,
       // 保存图片地址
       fileUrlList: [],
+      invoiceFileUrlList: [],
       tasks: this.initTasks(),
       buttonLoading: false,
       screenWidth: 0,
@@ -180,7 +215,11 @@ export default {
         reliable: '',
         member: [],
         teacher: []
-      }
+      },
+      // 保存预览图片路径
+      previewPath: '',
+      //预览窗口显示与影藏
+      previewVisible: false,
     }
   },
   computed: {
@@ -232,6 +271,7 @@ export default {
     },
     initUserRoles() {
       this.$get('system/user').then((r) => {
+        console.log(11, r)
         const userRoles = []
         const teacherRoles = []
         const rows = r.data.data.rows
@@ -241,7 +281,7 @@ export default {
             fullName: v.fullName
           }
           if (v.noteId === '1') teacherRoles.push(obj)
-          userRoles.push(obj)
+          else if (v.noteId === '2' || v.noteId === '3') userRoles.push(obj)
         })
         this.userRoles = userRoles
         this.teacherRoles = teacherRoles
@@ -256,20 +296,20 @@ export default {
     setTasks(val) {
       // 获得数据
       // 浅克隆，同一源里的数值也会改变
-      // this.editForm = row;
+      // this.tasks = row;
       this.tasks = Object.assign({}, val)
       this.team.reliable = val.reliable
       this.team.member = val.member
       this.team.teacher = val.teacher
       if (this.tasks.invoice.length > 1) {
-        this.fileUrlList = this.tasks.invoice.split(',')
+        this.invoiceFileUrlList = this.tasks.invoice.split(',')
       } else {
-        this.fileUrlList = this.tasks.invoice
+        this.invoiceFileUrlList = this.tasks.invoice
       }
-      for (var i = 0; i < this.fileUrlList.length; i++) {
-        var fileurl = this.fileUrlList[i]
+      for (var i = 0; i < this.invoiceFileUrlList.length; i++) {
+        var fileurl = this.invoiceFileUrlList[i]
         if (fileurl !== null || fileurl !== '') {
-          this.fileList.push({
+          this.invoiceFileList.push({
             name: fileurl.substring(28),
             url: fileurl
           })
@@ -292,15 +332,16 @@ export default {
           if (!this.tasks.itemsId) {
             // create
             // 调用getDge
+            const tasks = this.tasks
             const { a, b, flag } = this.getDge(this.team.reliable, this.team.member, this.team.teacher)
             if (flag) {
               this.buttonLoading = false
               return this.$message.info('不能多次选择同一个人，只能选择一次')
             }
             this.doSubmit()
-            this.tasks.userId = a
-            this.tasks.m_state = b
-            this.$post('studio/items', { ...this.tasks }).then(() => {
+            tasks.userId = a
+            tasks.m_state = b
+            this.$post('studio/items', { ...tasks }).then(() => {
               this.buttonLoading = false
               this.isVisible = false
               this.$message({
@@ -317,14 +358,22 @@ export default {
           } else {
             // update
             // 调用getDge
+            const tasks = this.tasks
             const { a, b, flag } = this.getDge(this.team.reliable, this.team.member, this.team.teacher)
             if (flag) {
               this.buttonLoading = false
               return this.$message.info('不能多次选择同一个人，只能选择一次')
             }
-            this.tasks.userId = a
-            this.tasks.m_state = b
-            this.$put('studio/items', { ...this.tasks }).then((r) => {
+            tasks.userId = a
+            tasks.m_state = b
+            delete tasks.members
+            console.log(22, tasks)
+            const updateState = {
+              state: tasks.state,
+              itemsId: tasks.itemsId
+            }
+            this.$put('studio/items/state', { ...updateState })
+            this.$put('studio/items', { ...tasks }).then((r) => {
               this.buttonLoading = false
               this.isVisible = false
               this.$message({
@@ -380,12 +429,17 @@ export default {
     handleSuccessInvoice(response, file, fileList) {
       const uid = file.uid
       const id = response.data.contentId
-      this.files.push({ uid, id })
+      this.invoiceFiles.push({ uid, id })
+      this.addInvoiceListLength++;
       if (this.tasks.invoice === '' || this.tasks.invoice === null) {
         this.tasks.invoice = response.data.url
       } else {
         this.tasks.invoice = this.tasks.invoice + ',' + response.data.url
       }
+      // this.editInvoiceFileList.push({
+      //   name: response.data.url.substring(28),
+      //   url: response.data.url,
+      // });
     },
     handleSuccessSpecification(response, file, fileList) {
       const uid = file.uid
@@ -395,6 +449,124 @@ export default {
         this.tasks.specification = response.data.url
       } else {
         this.tasks.specification = this.tasks.specification + ',' + response.data.url
+      }
+    },
+    // handleSuccessUrl(response, file, fileList) {
+    //   const uid = file.uid
+    //   const id = response.data.contentId
+    //   this.files.push({ uid, id })
+    //   if (this.tasks.url === '' || this.tasks.url === null) {
+    //     this.tasks.url = response.data.url
+    //   } else {
+    //     this.tasks.url = this.tasks.url + ',' + response.data.url
+    //   }
+    // },
+    // 删除添加表单上传的发票图片，写死的（图片长度限定为3）
+    addInvoiceBeforeRemove (file, fileList) {
+      let fileUrl = ''
+      let flag = false
+      // 添加
+      if (this.title === '新增') {
+         fileUrl = file.response.data.url
+         flag = true
+       } else {
+         // 修改
+         fileUrl = file.url
+       }
+      console.log(file,this.invoiceFileList)
+      if(flag) this.addInvoice(fileUrl, file)
+      else this.updateInvoice(file)
+       
+    },
+    addInvoice(fileUrl, file) {
+      for (let j = 0; j < this.invoiceFiles.length; j++) {
+        if (this.invoiceFiles[j].uid === file.uid) {
+          this.$delete(`oss/content/${this.invoiceFiles[j].id}`)
+          // 根据图片数量分别执行删除的功能
+          if (this.invoiceFiles.length === 1) {
+            console.log('111')
+            this.tasks.invoice = ''
+          } else if (this.invoiceFiles.length === 2) {
+            console.log('222');
+            if (j === 0) {
+              this.tasks.invoice = this.tasks.invoice.split(
+                fileUrl + ','
+              )[1]
+            } else {
+              this.tasks.invoice = this.tasks.invoice.split(
+                ',' + fileUrl
+              )[0]
+            }
+          } else {
+            if (j === 0) {
+              this.tasks.invoice = this.tasks.invoice.split(
+                fileUrl + ','
+              )[1];
+            } else if (j === 1) {
+              const firsturl = this.tasks.invoice.split(',' + fileUrl)[0]
+              const lasturl = this.tasks.invoice.split(',' + fileUrl)[1]
+              this.tasks.invoice = firsturl + lasturl
+            } else {
+              this.tasks.invoice = this.tasks.invoice.split(
+                ',' + fileUrl
+              )[0]
+            }
+          }
+          this.addInvoiceListLength--;
+          console.log(this.addInvoiceListLength)
+          // this.addInvoiceChange();
+          return true
+        }
+      }
+    },
+    updateInvoice(file) {
+      for (let n = 0; n < this.invoiceFileList.length; n++) {
+        if (this.invoiceFileList[n].uid === file.uid) {
+          const fileUrl = this.invoiceFileList[n].url;
+          const fileName = this.invoiceFileList[n].url
+            .substring(28)
+            .split('.')[0];
+          this.$delete(`oss/content`, { fileName: fileName });
+          // 根据图片数量分别执行删除的功能
+          if (this.invoiceFileList.length === 1) {
+            console.log('111');
+            this.tasks.invoice = '';
+            this.invoiceFileList = [];
+          } else if (this.invoiceFileList.length === 2) {
+            console.log('222');
+            if (n === 0) {
+              this.tasks.invoice = this.tasks.invoice.split(
+                fileUrl + ','
+              )[1];
+              this.invoiceFileList.shift();
+            } else {
+              this.tasks.invoice = this.tasks.invoice.split(
+                ',' + fileUrl
+              )[0];
+              this.invoiceFileList.pop();
+            }
+          } else {
+            if (n === 0) {
+              this.tasks.invoice = this.tasks.invoice.split(
+                fileUrl + ','
+              )[1];
+              this.invoiceFileList.shift();
+            } else if (n === 1) {
+              const firsturl = this.tasks.invoice.split(',' + fileUrl)[0];
+              const lasturl = this.tasks.invoice.split(',' + fileUrl)[1];
+              this.tasks.invoice = firsturl + lasturl;
+              this.invoiceFileList.splice(n, 1);
+            } else {
+              this.tasks.invoice = this.tasks.invoice.split(
+                ',' + fileUrl
+              )[0];
+              this.invoiceFileList.pop();
+            }
+          }
+          this.addInvoiceListLength--;
+          // this.editInvoiceChange();
+          return true;
+        }
       }
     },
     handleBeforeRemove(file, fileList) {
@@ -410,7 +582,25 @@ export default {
         }
       }
     },
-    handleBeforeUpload(file) {
+    handleBeforeUploadInvoice(file) {
+      if (file.size / 1024 > 5000) {
+        this.$message({
+          message: '上传文件大小不能超过5MB!',
+          type: 'error'
+        })
+        return false
+      } else {
+        const ext = file.name.replace(/.+\./, '')
+        if (!validatePicExt(ext)) {
+          this.$message({
+            type: 'error',
+            message: '禁止上传' + ext + '类型的附件'
+          })
+          return false
+        }
+      }
+    },
+    handleBeforeUploadSpecif(file) {
       if (file.size / 1024 > 5000) {
         this.$message({
           message: '上传文件大小不能超过5MB!',
@@ -428,12 +618,47 @@ export default {
         }
       }
     },
+    // handleBeforeUploadUrl(file) {
+    //   if (file.size / 1024 > 10000) {
+    //     this.$message({
+    //       message: '上传文件大小不能超过10MB!',
+    //       type: 'error'
+    //     })
+    //     return false
+    //   } else {
+    //     const ext = file.name.replace(/.+\./, '')
+    //     if (!validateFileExt(ext)) {
+    //       this.$message({
+    //         type: 'error',
+    //         message: '禁止上传' + ext + '类型的附件'
+    //       })
+    //       return false
+    //     }
+    //   }
+    // },
     // 刷新上传列表
     doSubmit() {
       this.fileList = []
       this.fileUrlList = []
+      this.invoiceFileList = [];
+      this.invoiceFiles = [];
       this.dialogImageUrl = ''
       this.dialog = false
+    },
+    // 修改时的图片预览效果
+    handlePreview(file) {
+      console.log(file)
+      if ('url' in file) {
+        this.previewPath = file.url
+      } else {
+        this.previewPath = file.response.data.url
+      }
+      this.previewVisible = true
+    },
+    // 图片预览关闭
+    previewDialogClose() {
+      this.previewPath = ''
+      this.previewVisible = false
     }
   }
 }
